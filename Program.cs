c4ee42e9c6ac7a744753149d19fe7646d24a9767
HenryKasai <henrykasai1775@gmail.com>
2020-02-28 21:51:57 -0300

    Solução de exercício

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace M_sica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Música Fuego");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Soy el fuego que");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Param ram ram ram");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Pa rã rã rã ram");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("tã nã nã nã nã");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("tã nã nã nã nã");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Soy el fuego que");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Param ram ram ram");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Pa rã rã rã ram");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("tã nã nã nã nã");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("tã nã nã nã nã");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # M-sica
 Programa que apresenta a letra de uma música
+
+Solução do exercício:
+
+Faça um programa que exiba uma letra de música a sua escolha. Utilize cores em destaque no título e nos refrões.
